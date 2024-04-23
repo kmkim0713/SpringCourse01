e59550bf7e571d0b59d010d2a9dd061edcc67b13
kyungminkim <kmkimworkwork@gmail.com>
2024-04-23 15:08:54 +0900

    AppConfig를 이용한 테스트 코드 작성

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
 //    MemberService memberService = new MemberServiceImpl();
 
+    MemberService memberService;
+
+    // test 코드를 작성할 때는 직접 생성이나 AppConfig를 사용하는 것 보다
+    // @BeforeEach를 사용하자. @BeforeEach는 각 테스트를 실행하기전에 무조건 실행하는 어노테이션
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
